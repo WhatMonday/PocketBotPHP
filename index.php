23038e7ed50db8b935340263b7227862a5aae8a6
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-10-30 18:37:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
        // if($message == "สวัสดี"){
             $topic = "node-red";
             pubMqtt($topic,$message);
-      //      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";}
+            $arrayPostData['messages'][0]['text'] = $message;}
      //   else if($message == "ลาก่อน"){
 
     //        $arrayPostData['messages'][0]['text'] = "โชคดีนะ";}
