90036771009587eb46d342da7e59f131619b270c
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-10-30 14:44:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
     $arrayPostData['messages'][0]['type'] = "text";
         if($message == "สวัสดี"){
-            //pubMqtt("phphook","Hello")
+            pubMqtt("node1","Hello")
             $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";}
         else if($message == "ลาก่อน"){
             //pubMqtt("phphook","GoodBye")
