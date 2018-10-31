185bd4ea3ba24f1a6897429942794b159608323b
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-10-31 15:38:47 +0700

    Update reply.php

--- reply.php
+++ reply.php
@@ -1,26 +1,25 @@
 <?php
- 
 $strAccessToken = "dzaOAQEbS4W3KQFaoq2IbC8Z6rxrvk46MkI6tgcmhFRy9amJTG48myOZdg8OuKsex4aKxDgevUajHk9PgtXLR1GTjlFav5brcEKP8bV/o+YqkSeVylPHY+UtfzzNrZO4OT6ZGZSfa3cFvpNMosmuRQdB04t89/1O/w1cDnyilFU=";
- 
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
- 
-$arrPostData = array();
+$arrayHeader = array();
+$arrayHeader[] = "Content-Type: application/json";
+$arrayHeader[] = "Authorization: Bearer {$strAccessToken}";
 $arrayPostData['replyToken'] = "47f936df88d04dda857381c90dc9d634";
 $arrayPostData['messages'][0]['type'] = "text";
 $arrayPostData['messages'][0]['text'] = "OK";
+replyMsg($arrayHeader,$arrayPostData);
 
-
+function replyMsg($arrayHeader,$arrayPostData){
     $strUrl = "https://api.line.me/v2/bot/message/reply";
     $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,$strUrl);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);    
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
         curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($ch);
         curl_close ($ch);
+    }
+   exit;
 ?>
