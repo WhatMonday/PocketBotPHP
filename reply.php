7a2627ab7792a9c23210510e2c388e6318ecf1a4
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-01 10:15:27 +0700

    Update reply.php

--- reply.php
+++ reply.php
@@ -1,6 +1,6 @@
 <?php
-$msg=$_POST["message"];
-$token=$_POST["token"];
+$msg=$_GET["message"];
+$token=$_GET["token"];
 echo $msg;
 echo $token;
 $strAccessToken = "dzaOAQEbS4W3KQFaoq2IbC8Z6rxrvk46MkI6tgcmhFRy9amJTG48myOZdg8OuKsex4aKxDgevUajHk9PgtXLR1GTjlFav5brcEKP8bV/o+YqkSeVylPHY+UtfzzNrZO4OT6ZGZSfa3cFvpNMosmuRQdB04t89/1O/w1cDnyilFU=";
