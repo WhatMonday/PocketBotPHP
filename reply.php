0d833c471e3bbe7ab8119998b9f59bb1c5d20d28
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-09 15:25:32 +0700

    Update reply.php

--- reply.php
+++ reply.php
@@ -1,15 +1,14 @@
 <?php
 $msg=$_GET["message"];
 $token=$_GET["token"];
-echo $msg;
-echo $token;
+$device = array("D0","D1","D2","D3","D4","D5","D6","D7","D8");
  $accessToken = "dzaOAQEbS4W3KQFaoq2IbC8Z6rxrvk46MkI6tgcmhFRy9amJTG48myOZdg8OuKsex4aKxDgevUajHk9PgtXLR1GTjlFav5brcEKP8bV/o+YqkSeVylPHY+UtfzzNrZO4OT6ZGZSfa3cFvpNMosmuRQdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
     $arrayHeader = array();
         $arrayHeader[] = "Content-Type: application/json";
         $arrayHeader[] = "Authorization: Bearer {$accessToken}";
     $arrayPostData['replyToken'] = $token;
     $arrayPostData['messages'][0]['type'] = "text";
-    $arrayPostData['messages'][0]['text'] = $msg;
+    $arrayPostData['messages'][0]['text'] = ConvertMessage($msg);    
 replyMsg($arrayHeader,$arrayPostData);
 function replyMsg($arrayHeader,$arrayPostData){
     $strUrl = "https://api.line.me/v2/bot/message/reply";
@@ -24,4 +23,22 @@ function replyMsg($arrayHeader,$arrayPostData){
     $result = curl_exec($ch);
         curl_close ($ch);
     }
+function ConvertMessage($msg){
+   $s = explode(",", $msg);
+   $result=$device[(int)$s[0]];
+    if ($s[2] == "check"){
+         if ($s[1] == "on"){
+             $result .= " ถูกเปิดอยู่";
+        }else if($s[1] == "off"){
+             $result .= " ถูกปิดอยู่";
+        }
+    }else{ 
+        if ($s[2] == "on"){
+             $result .= " ถูกเปิดแล้ว";
+        }else if($s[2] == "off"){
+             $result .= " ถูกปิดแล้ว";
+        }
+    }
+    return $result;
+}
 ?>
